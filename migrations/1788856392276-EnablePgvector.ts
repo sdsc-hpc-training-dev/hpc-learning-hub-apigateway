9ee20de2de3b585c3ef5ab7820d3f2242b9363ee
Srujam Dave <srdave@ucsd.edu>
2026-09-08 01:38:45 -0700

    Add first database migration

---
+++ migrations/1788856392276-EnablePgvector.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class EnablePgvector1788856392276 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS vector');
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('DROP EXTENSION IF EXISTS vector');
+    }
+
+}
