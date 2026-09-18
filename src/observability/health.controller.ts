6d847b2e0fb997bd3c648fb5977f488ba3fee815
Srujam Dave <srdave@ucsd.edu>
2026-09-18 00:29:39 -0700

    Add health endpoint

---
+++ src/observability/health.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get } from '@nestjs/common'
+import {
+    HealthCheckResult,
+    HealthCheckService,
+    TypeOrmHealthIndicator
+} from '@nestjs/terminus'
+
+@Controller()
+export class HealthController {
+    constructor(
+        private readonly health: HealthCheckService,
+        private readonly database: TypeOrmHealthIndicator,
+    ){}
+
+    @Get('health')
+    async getHealthInfo(): Promise<Object> {
+        const result = await this.health.check([
+            () => this.database.pingCheck('postgres').withTimeout(1500)
+        ]);
+
+        return {
+            healthy: result.status === 'ok',
+            timestamp: new Date(),
+            services: {
+                postgres: result.details.postgres.status,
+            }
+        }
+    }
+}
--- src/observability/observability.module.ts
+++ src/observability/observability.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TelemetryService } from './telemetry.service';
+import { HealthController } from './health.controller';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
+  controllers: [HealthController],
   providers: [TelemetryService],
+  imports: [TerminusModule],
   exports: [TelemetryService],
 })
 export class ObservabilityModule {}
